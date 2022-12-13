5e5b5a12d2fd98bf391675f7229c051caa7bbd23
RahulNeupane <nrahul503@gmail.com>
2022-12-13 20:44:52 +0545

    test-commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// new comment added 
 const toggleSwitch = document.querySelector('input[type="checkbox"]')
 const nav = document.getElementById('nav')
 const toggleIcon = document.getElementById('toggle-icon')
